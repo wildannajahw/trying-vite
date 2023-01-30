ae2395ac6cedb261d4ba9b9e530bb20301f165a5
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2023-01-30 14:16:31 +0700

    installing tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import {QueryClientProvider} from '@tanstack/react-query/build/lib/QueryClientProvider';
+import getQueryClient from './utils/getQueryClient';
+
 function App() {
+	const queryClient = getQueryClient();
 	return (
-		<div className='App'>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className='App'>
+				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			</div>
+		</QueryClientProvider>
 	);
 }
 
